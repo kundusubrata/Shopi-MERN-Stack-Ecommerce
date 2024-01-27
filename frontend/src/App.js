c747f203cf246c1dee2f13060b4fe3608f4c856f
kundusubrata <kundu.subrata2020@outlook.com>
2024-01-27 16:15:31 +0530

    05-frontend-cleaning-project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/layout/Header.jsx';
+import Footer from './components/layout/Footer.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
